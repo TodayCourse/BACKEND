162e4b19450323060220ec8d509a9c2795c997c7
Choiyu330 <jenny4063@naver.com>
2025-06-14 15:15:03 +0900

    feat: 카테고리 테스트 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TRAVEL_CATEGORY(category_id, category_name, sort_order, reg_user_id, reg_dtt) VALUES (1, '카테고리1', 1, 'test', NOW());
+INSERT INTO TRAVEL_CATEGORY(category_id, category_name, sort_order, reg_user_id, reg_dtt) VALUES (2, '카테고리2', 2, 'test', NOW());
+INSERT INTO TRAVEL_CATEGORY(category_id, category_name, sort_order, reg_user_id, reg_dtt) VALUES (3, '카테고리3', 3, 'test', NOW());
