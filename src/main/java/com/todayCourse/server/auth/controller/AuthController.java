3f983759e92a5a146acc53a723ea9ba67cb9d25e
Choiyu330 <jenny4063@naver.com>
2025-11-01 17:51:30 +0900

    feat: refreshToken SamSite 옵션 추가

--- src/main/java/com/todayCourse/server/auth/controller/AuthController.java
+++ src/main/java/com/todayCourse/server/auth/controller/AuthController.java
@@ -47,6 +47,7 @@ public class AuthController {
                 .secure(true)
                 .path("/")
                 .maxAge(24 * 60 * 60) // 1일
+                .sameSite("None")
                 .build();
 
         response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
@@ -71,6 +72,7 @@ public class AuthController {
                 .secure(true)
                 .path("/")
                 .maxAge(24 * 60 * 60) // 1일
+                .sameSite("None")
                 .build();
 
         response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
